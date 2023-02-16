fe1dc2adbb687713e2f80986f955ff65ef522126
muthaphuli <u13079248@tus.co.za>
2023-02-16 22:15:26 +0200

    write c scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Prints quote
+ *
+ * Return: zero on sucess
+ *
+ */
+int main(void)
+{	
+	printf("with proper grammar, but the coutcome is a piece of art.\n");
+	return(0)
+}	
