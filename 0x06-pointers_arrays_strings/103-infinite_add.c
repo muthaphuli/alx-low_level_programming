d2cbfbf5d84efa2a1699d525b2e8b057eb466086
winston <u13079248@tus.co.za>
2023-03-01 20:03:19 +0200

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * reverse - reverses a string
@@ -33,17 +34,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
+
+	char *temp = malloc(sizeof(char) * (len1 + len2 + 1));
 	int i, j, k, sum, carry = 0;
 
-	/**
-	 *  if the result buffer is too small, return 0 
-	 */
-	if (len1 + len2 > size_r)i
-		return (0);
-
-	/**
-	 *  Perform addition
-	 */
+	/* Perform addition */
 	for (i = len1 - 1, j = len2 - 1, k = 0;
 		i >= 0 || j >= 0 || carry;
 		i--, j--, k++)
@@ -51,25 +46,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		sum = carry;
 		sum += (i >= 0) ? n1[i] - '0' : 0;
 		sum += (j >= 0) ? n2[j] - '0' : 0;
-		r[k] = sum % 10 + '0';
+		temp[k] = sum % 10 + '0';
 		carry = sum / 10;
 	}
 
-	/* 
-	 *if the result is empty, return 0 
-	 */
+	/* if the result is empty, return 0 */
 	if (k == 0)
 		return (0);
 
-	/*
-	 *null terminate the result 
-	 */
-	r[k] = '\0';
+	/* null terminate the result */
+	temp[k] = '\0';
 
-	/*
-	 *reverse the result string
-	 */
-	reverse(r);
+	/* if the result is too big, return 0 */
+	if ((int)strlen(temp) + 1 > size_r)
+		return (0);
 
-	return (r);
-}i
+	r = strcpy(r, temp);
+	free(temp);
+
+	/* return the reversed result */
+	return (reverse(r));
+}
