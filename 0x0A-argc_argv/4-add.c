7f7daf5cfdd57a713a841d6b28de98e7a937df02
winston <u13079248@tus.co.za>
2023-03-16 05:36:59 +0200

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
- *  * main - adds positive numbers.
- *   * @argc: number of command line arguments.
- *    * @argv: array that contains the program command line arguments.
- *     * Return: 0 - success.
- *      */
+ * main - add positive numbers.
+ * @argc: count arguments passed in argv
+ * @argv: String that holds the passed Arguments.
+ *
+ * Return: 0 (Sucess) else 1 (Failure).
+ */
 int main(int argc, char *argv[])
 {
-		int i, j, add = 0;
+	int i, c, sum = 0;
 
-			for (i = 1; i < argc; i++)
-					{
-								for (j = 0; argv[i][j] != '\0'; j++)
-											{
-															if (!isdigit(argv[i][j]))
-																			{
-																								printf("Error\n");
-																												return (1);
-																															}
-																	}
-										add += atoi(argv[i]);
-											}
-				printf("%d\n", add);
-					return (0);
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (c = 0; argv[i][c] != '\0'; c++)
+		{
+			if (!isdigit(argv[i][c]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
 }
