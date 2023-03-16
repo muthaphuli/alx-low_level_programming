de74d9cdbf5335052f43de34678332f62f9aef15
winston <u13079248@tus.co.za>
2023-03-16 05:40:56 +0200

    print all arguets

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
- *  * main - prints all arguments it receives.
- *   * @argc: number of command line arguments.
- *    * @argv: array that contains the program command line arguments.
- *     * Return: 0 - success.
- *      */
+ * main - Entry point, prints the arguments plus the application name.
+ * @argc: count.
+ * @argv: string with the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
-
 {
-		int i;
+	int i;
 
-			for (i = 0; i < argc; i++)
-						printf("%s\n", argv[i]);
-				return (0);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
 }
