98c61305a4126724f4519909587deccf802f6dc3
winston <u13079248@tus.co.za>
2023-03-20 21:44:17 +0200

    defining variables and their types

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,19 @@
-#ifndef _DOG_H
-#define _DOG_H
-
-#include "main.h"
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * struct dog - info on dog.
- * @name: dog's name.
- * @age: age of the dog in float.
- * @owner: name of the owner of the dog.
- *
- */
-struct dog
+ * struct dog - dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ **/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/* function for typedef dog_t */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
