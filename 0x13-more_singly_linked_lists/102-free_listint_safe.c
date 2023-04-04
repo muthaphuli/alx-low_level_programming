4d3db02d54b02f243a955af98d59214a34ae22f7
winston <u13079248@tus.co.za>
2023-04-04 13:39:47 +0200

    task3

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,24 +1,11 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
-
 /**
- * free_listint_safe - free a listint_t list, including loops
- * @head: address of head of list
- * Return: size of list freed
- **/
+  * free_listint_safe - define function
+  * @h:
+  * Return: what does it return?
+  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *friend, start;
-	size_t i;
-
-	start = head;
-	for (i = 0; head != NULL && head != start; i++)
-	{
-		friend = head->next;
-		free(head);
-		head = friend;
-	}
-	return (i);
+	h++;
+	return (0);
 }
