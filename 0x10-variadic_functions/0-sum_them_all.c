316f10b29b7846a074c342cd0bd1f738aedf9e42
Winston <winston@HUAWEI_Y6_2019-75d8488909.localdomain>
2023-08-17 21:54:41 +0200

    sum between functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- *first question 
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
  * Return: sum of all parameters
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 
 /**
  * print_numbers - prints numbers, followed by a new line
- * @separator: string to be printed between numbers
+ * @separator: string to be printed between all numbers
  * @n: number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
