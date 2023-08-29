6124d95c9f54d3684f6ad81494e2f28644cce00e
Winston <winston@HUAWEI_Y6_2019-75d8488909.localdomain>
2023-08-29 13:34:01 +0200

    reverse list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,77 +1,26 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * looped_listint_len - calculates the length of a loop in a linked list
- * @head: pointer to the head of the linked list
- *
- * Return: the number of nodes in the loop, or 0 if there is no loop
+ * reverse_listint - reverses a listint_t linked list
+ * @head: double pointer to the head of the list
+ * Return: pointer to the first node of the reversed list
  */
-size_t looped_listint_len(const listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	const listint_t *slow_ptr, *fast_ptr;
-	size_t length = 1;
+	listint_t *prev, *next;
 
-	/* If the list is empty or has only one node, there is no loop */
-	if (head == NULL || head->next == NULL)
-		return (0);
+	if (head == NULL || *head == NULL)
+		return (NULL);
 
-	slow_ptr = head->next;  /* tortoise pointer */
-	fast_ptr = head->next->next;  /* hare pointer */
-
-	/* Floyd's cycle-finding algorithm */
-	while (fast_ptr != NULL)
+	prev = NULL;
+	while (*head)
 	{
-		if (slow_ptr == fast_ptr)
-		{
-			slow_ptr = head;
-			while (slow_ptr != fast_ptr)
-			{
-				length++;
-				slow_ptr = slow_ptr->next;
-				fast_ptr = fast_ptr->next;
-			}
-
-			slow_ptr = slow_ptr->next;
-			while (slow_ptr != fast_ptr)
-			{
-				length++;
-				slow_ptr = slow_ptr->next;
-			}
-
-			return (length);
-		}
-
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
+	*head = prev;
 
-	return (0);
-}
-
-/**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: The number of nodes in the list.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t length, i;
-
-	length = looped_listint_len(head);
-
-	if (!length)
-	{
-		for (; head != NULL; length++, head = head->next)
-			printf("[%p] %d\n", (void *) head, head->n);
-	}
-	else
-	{
-		for (i = 0; i < length; i++, head = head->next)
-			printf("[%p] %d\n", (void *) head, head->n);
-		printf("-> [%p] %d\n", (void *) head, head->n);
-	}
-
-	return (length);
+	return (*head);
 }
