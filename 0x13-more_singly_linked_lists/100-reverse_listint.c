fd291b4133cba259fcb5b7204fc71dcd44f41cf0
Winston <winston@Winstons-MacBook-Air.local>
2023-08-29 12:18:16 +0200

    100 file

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
 /**
+ *
  * reverse_listint - reverses a listint_t linked list
  * @head: double pointer to the head of the list
  * Return: pointer to the first node of the reversed list
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
 /**
+ *
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node, starting at 0
